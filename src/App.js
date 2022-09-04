7931cf66eeeb327d0e273dfdecfcb381c1f63b52
desipossa <c1jang@hanmail.net>
2022-09-04 12:17:00 +0900

    20220904

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './component/Header';
+import MainVisual from './component/MainVisual'
+import Portfolio from './component/Portfolio';
+
+function App() {
+  return (
+    <div className="Wrap">
+      <Header />
+      <MainVisual />
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
